ca85c064d543fd2dd89144a792cfa0220c9db29a
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-18 00:56:18 +0200

    got the item prices

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ driver.get(url="http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(by=By.ID, value="cookie")
 
 # find the right panel store
-store = driver.find_elements(by=By.CSS_SELECTOR, value="#rightPanel #store div")
+store = driver.find_elements(by=By.CSS_SELECTOR, value="#rightPanel #store div b")
 # find each item within the store
 store_items = [item.text for item in store]
-for item in store_items:
-    print(item)
+
+del store_items[-1]
+# for item in store_items:
+#     cost = item.split("-")[1].strip()
+#     print(cost)
+items = [item.split(" - ") for item in store_items]
+print(items)
 
 
 
