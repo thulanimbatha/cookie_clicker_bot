0394052de4b80bb0282fd1515414d4a73ea8bb71
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-17 15:00:19 +0200

    changed url - easier to locate cookie

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+import time
 
 service = Service("C:\Development\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get(url="https://orteil.dashnet.org/cookieclicker/")
+driver.get(url="http://orteil.dashnet.org/experiments/cookie/")
 
 # find the cookie
-cookie = driver.find_element(by=By.ID, value="bigCookie")
+cookie = driver.find_element(by=By.ID, value="cookie")
+
+
+
+
 
