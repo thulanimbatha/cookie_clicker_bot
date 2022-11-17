a43e9711e1bcff31d33f31a4e76c925325db719a
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-17 12:01:50 +0200

    found cookie

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ service = Service("C:\Development\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get(url="https://orteil.dashnet.org/cookieclicker/")
+
+# find the cookie
+cookie = driver.find_element(by=By.CSS_SELECTOR, value="#cookieAnchor #bigCookie")
+
