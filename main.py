03087c879c65ba79fe8f79dc890c30067ff3e3f2
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-18 02:16:17 +0200

    added interval checker to check points after every 5 secs - makes the game run faster

--- main.py
+++ main.py
@@ -14,32 +14,45 @@ cookie = driver.find_element(by=By.ID, value="cookie")
 
 index = 0
 
+interval_check = time.time() + 5
+
+# TODO Finish final part of project - adding in the click counter, stopping game after 5 minutes
+
 while True:
     cookie.click()
 
-    # find the right panel store
-    store = driver.find_elements(by=By.CSS_SELECTOR, value="#rightPanel #store div b")
-    # find each item within the store
-    store_items = [item.text for item in store]
-    # delete useless item
-    del store_items[-1]
-    # get the split items
-    items = [item.split(" - ") for item in store_items]
-    # get the costs
-    costs = [item[1] for item in items]
-    for cost in costs:
-        if cost != "":
-            cost.replace(",", "")
+    # check every 10 seconds
+    if time.time() > interval_check:
 
-    points = driver.find_element(by=By.ID, value="money").text
-    if points != "":
-        points.replace(",", "")
+        # find the right panel store
+        store = driver.find_elements(by=By.CSS_SELECTOR, value="#rightPanel #store div b")
+        
+        # find each item within the store
+        store_items = [item.text for item in store]
+        
+        # delete useless item
+        if store_items[-1] == "":
+            del store_items[-1] 
+        
+        # get the split items
+        
+        items = [item.split(" - ") for item in store_items]
+        
+        # get the costs
+        costs = [item[1] for item in items]
+        for cost in costs:
+            if cost != "":
+                cost.replace(",", "")
 
-    if int(points) > int(costs[index]):
-        # click on highest affordable element
-        store[index].click()
-        # move onto next item in store
-        index += 1
+        points = driver.find_element(by=By.ID, value="money").text
+        if points != "":
+            points.replace(",", "")
+
+        if int(points) > int(costs[index]):
+            # click on highest affordable element
+            store[index].click()
+            # move onto next item in store
+            index += 1
 
 
 
